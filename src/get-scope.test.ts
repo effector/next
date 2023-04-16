1a693dec2454040075bc874836097f3ea633df51
AlexandrHoroshih <xad71rus@gmail.com>
2023-04-17 00:28:07 +0700

    Update test

--- src/get-scope.test.ts
+++ src/get-scope.test.ts
@@ -6,6 +6,7 @@ import {
   fork,
   serialize,
   allSettled,
+  combine,
   sample,
 } from "effector";
 
@@ -15,11 +16,18 @@ const up = createEvent();
 const longUpFx = createEffect(async () => {
   await new Promise((r) => setTimeout(r, 10));
 });
-const $count = createStore(0, { sid: "$count" }).on(
+const $count = createStore(0).on(
   [up, longUpFx.done],
   (s) => s + 1
 );
 const $derived = $count.map((s) => ({ ref: s }));
+const $combined = combine({ ref: $count });
+const $nestedCombined = combine({ ref: $derived });
+
+const $sampled = sample({
+  source: { ref: $combined },
+  fn: (ref) => ref.ref.ref,
+});
 
 describe("getClientScope", () => {
   test("should handle server values injection on the fly", async () => {
@@ -35,6 +43,9 @@ describe("getClientScope", () => {
 
     expect(clientScopeOne.getState($count)).toEqual(0);
     expect(clientScopeOne.getState($derived)).toEqual({ ref: 0 });
+    expect(clientScopeOne.getState($combined)).toEqual({ ref: 0 });
+    expect(clientScopeOne.getState($nestedCombined)).toEqual({ ref: { ref: 0 } });
+    expect(clientScopeOne.getState($sampled)).toEqual(0);
     expect(clientScopeOne.getState(longUpFx.inFlight)).toEqual(0);
 
     const promise = allSettled(longUpFx, { scope: clientScopeOne });
@@ -45,12 +56,18 @@ describe("getClientScope", () => {
 
     expect(clientScopeTwo.getState($count)).toEqual(3);
     expect(clientScopeOne.getState($derived)).toEqual({ ref: 3 });
+    expect(clientScopeOne.getState($combined)).toEqual({ ref: 3 });
+    expect(clientScopeOne.getState($nestedCombined)).toEqual({ ref: { ref: 3 } });
+    expect(clientScopeOne.getState($sampled)).toEqual(3);
     expect(clientScopeOne.getState(longUpFx.inFlight)).toEqual(1);
 
     await promise;
 
     expect(clientScopeTwo.getState($count)).toEqual(4);
     expect(clientScopeOne.getState($derived)).toEqual({ ref: 4 });
+    expect(clientScopeOne.getState($combined)).toEqual({ ref: 4 });
+    expect(clientScopeOne.getState($nestedCombined)).toEqual({ ref: { ref: 4 } });
+    expect(clientScopeOne.getState($sampled)).toEqual(4);
     expect(clientScopeOne.getState(longUpFx.inFlight)).toEqual(0);
   });
 });
