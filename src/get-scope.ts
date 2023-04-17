2cbf43cc860d708c8536aba1c8cb08627c9c6c07
AlexandrHoroshih <xad71rus@gmail.com>
2023-04-17 22:16:21 +0700

    Handle shallow navigation edge case

--- src/get-scope.ts
+++ src/get-scope.ts
@@ -16,13 +16,31 @@ function getServerScope(values?: Values) {
  * This temporary solution on hacks allows us to solve the pain of library users when working with Next.js, as well as gather much more information to develop a better API.
  */
 const _currentScope: Scope = fork();
+let prevValues: Values;
 /**
  * @private
  *
  * exported for tests only
  */
 export function getClientScope(values?: Values) {
-  if (!values) return _currentScope;
+  if (
+    !values ||
+    /**
+     * This is a hack to handle edge cases with shallow navigation
+     *
+     * In this case Next.js will basically re-use old pageProps,
+     * but we already have latest state in the client scope
+     *
+     * So this update is just skipped
+     */
+    values === prevValues
+  )
+    return _currentScope;
+
+  /**
+   * Saving previous values to handle edge cases with shallow navigation
+   */
+  prevValues = values;
 
   HACK_injectValues(_currentScope, values);
   HACK_updateScopeRefs(_currentScope, values);
