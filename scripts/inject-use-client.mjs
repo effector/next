3143367470f2878e287c0fb15da973bf1612e0be
AlexandrHoroshih <xad71rus@gmail.com>
2023-05-08 17:04:38 +0700

    Add script to inject use client directive

---
+++ scripts/inject-use-client.mjs
@@ -0,0 +1,25 @@
+import "zx/globals";
+
+import { resolve } from "node:path";
+
+const DIRECTIVE = `'use client';`;
+
+const pkgJson = await fs.readJson(resolve("package.json"));
+
+const indexFilePaths = Object.values(pkgJson.exports["."]).map((path) =>
+  resolve(path)
+);
+
+for (const path of indexFilePaths) {
+  console.log(chalk.blue(`Injecting ${DIRECTIVE} into ${path}`));
+  await injectUseClient(path);
+  console.log(chalk.green(`Injected ${DIRECTIVE} into ${path}`));
+}
+
+async function injectUseClient(path) {
+  const content = await fs.readFile(path, "utf-8");
+
+  if (content.startsWith(DIRECTIVE)) return;
+
+  await fs.writeFile(path, `${DIRECTIVE}\n\n${content}`);
+}
